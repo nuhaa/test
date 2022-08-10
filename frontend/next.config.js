16ee65856e20da21435ecb10ee5ec1ae99b6d3c2
Muhammad Ulin Nuha <nuha.um@gmail.com>
2022-08-10 18:51:03 +0700

    next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,28 @@ const nextConfig = {
   publicRuntimeConfig: {
     backendUrl: process.env.NEXT_PUBLIC_BASE_URL_BACKEND,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://devel.bebasbayar.com/:path*',
+      },
+    ]
+  },
+  async headers() {
+      return [
+        {
+          // matching all API routes
+          source: "/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+  },
 }
 
 module.exports = nextConfig
